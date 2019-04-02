bac3147f784a08c24d09cffc92c55fd16abfbf2c
foarsitter <info@jelmert.nl>
2019-04-02 23:00:06 +0200

    Added webapp and readme

---
+++ READMe.md
@@ -0,0 +1,7 @@
+The webapp directory contains a Flask webapp with the remote. 
+
+KEY_1, KEY_2, KEY_3 and KEY_4 are the time control buttons, 
+
+KEY_F* are the pathfinding algoritms
+
+KEY_KP* are for the brushes on the front. 
---
+++ webapp/app.py
@@ -0,0 +1,43 @@
+from functools import wraps
+from flask import Flask, request, redirect, render_template, Response
+import os
+
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == 'pi' and password == 'raspberry'
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+@requires_auth
+def index():
+
+    if request.method == 'POST':
+        command_str = "irsend SEND_ONCE fc8822 {}".format(request.form['button'])
+        print(command_str)
+        os.system(command_str)
+        return redirect('/')
+        
+    
+    return render_template('app.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
